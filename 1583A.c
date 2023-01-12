6a63d06e10f7903e26282714d67459dd13da2bf8
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2023-01-12 15:27:12 +0600

    Add files via upload

--- 1583A.c
+++ 1583A.c
@@ -19,30 +19,34 @@ int main(){
         }
         if(flag==1){
             printf("%d\n",n);
-            for(int j=0;j<n;j++){
-                printf("%d ",j+1);
+            for(int j=1;j<=n;j++){
+                printf("%d ",j);
             }
         }
         else{
             int sum2;
             for(int j=0;j<n;j++){
                 sum2=sum-a[j];
-                int flag=0;
-                for(int j=2;j<=sum2/2;j++){
-                    if(sum2%j==0){
-                        flag=1;
+                int flag2=0;
+                for(int k=2;k<=sum2/2;k++){
+                    if(sum2%k==0){
+                        flag2=1;
                         break;
                     }
                 }
-                if(flag==1){
+                if(flag2==1){
                     printf("%d\n",n-1);
-                    for(int j=0;j<n-1;j++){
-                        printf("%d ",j+1);
+                    for(int k=0;k<n;k++){
+                        if(k==j){
+                            continue;
+                        }
+                        printf("%d ",k+1);
                     }
                     break;
                 }
             }
         }
+        printf("\n");
     }
     return 0;
 }
